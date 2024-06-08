ef5604073fed601c7b053a47212bacae49618048
Rahul <chattychidiya@gmail.com>
2024-06-08 15:59:53 +0530

    chore: Fix z indez for navigation bar

--- src/components/Aceternity/background-gradient.tsx
+++ src/components/Aceternity/background-gradient.tsx
@@ -68,7 +68,7 @@ export const BackgroundGradient = ({
 				)}
 			/>
 
-			<div className={cn("relative z-10", className)}>{children}</div>
+			<div className={cn("relative z-[1]", className)}>{children}</div>
 		</div>
 	);
 };
