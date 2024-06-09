37dfcaee133cf1e957f8ed8ada79e6ca8395f796
Rahul <chattychidiya@gmail.com>
2024-06-09 13:20:10 +0530

    feat: Add mongoDB

---
+++ src/model/skill.model.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+export interface Skill {
+	name: string;
+	logo: string;
+	category: string;
+	invert: boolean;
+}
+
+const SkillSchema = new mongoose.Schema<Skill>({
+	name: {
+		type: String,
+		required: true,
+	},
+	logo: {
+		type: String,
+		required: true,
+	},
+	category: {
+		type: String,
+		required: true,
+	},
+	invert: {
+		type: Boolean,
+		required: false,
+		default: false,
+	},
+});
+
+const SkillModel =
+	(mongoose.models.Skill as mongoose.Model<Skill>) ||
+	mongoose.model<Skill>("Skill", SkillSchema);
+
+export default SkillModel;
