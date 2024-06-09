48c1e0f9bdc574c11c3af55afd654a9e45eca52a
Rahul <chattychidiya@gmail.com>
2024-06-09 15:43:17 +0530

    chore: Code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,31 +9,25 @@ import ProjectModel, { Project } from "@/model/projects.model";
 export default async function Home() {
 	await dbConnect();
 
-	const skills: Skill[] = await SkillModel.find({})
-		.lean()
-		.then((skills: Skill[]) =>
-			skills.map((skill) => ({
-				name: skill.name,
-				logo: skill.logo,
-				category: skill.category,
-				invert: skill.invert,
-			})),
-		);
+	const skillData = await SkillModel.find({}).lean();
+	const skills: Skill[] = skillData.map((skill) => ({
+		name: skill.name,
+		logo: skill.logo,
+		category: skill.category,
+		invert: skill.invert,
+	}));
 
 	const categories = Array.from(new Set(skills.map((skill) => skill.category)));
 
-	const projects: Project[] = await ProjectModel.find({})
-		.lean()
-		.then((projects: Project[]) =>
-			projects.map((project) => ({
-				title: project.title,
-				description: project.description,
-				techStack: project.techStack,
-				logo: project.logo,
-				websiteLink: project.websiteLink,
-				githubLink: project.githubLink,
-			})),
-		);
+	const projectData = await ProjectModel.find({}).lean();
+	const projects: Project[] = projectData.map((project) => ({
+		title: project.title,
+		description: project.description,
+		techStack: project.techStack,
+		logo: project.logo,
+		websiteLink: project.websiteLink,
+		githubLink: project.githubLink,
+	}));
 
 	return (
 		<div>
