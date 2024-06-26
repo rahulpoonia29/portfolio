a21301871bd0609548afcf3a5d02b4690f7d3a9f
Rahul <chattychidiya@gmail.com>
2024-06-26 13:58:22 +0530

    chore: force dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-	darkMode: "class",
+	darkMode: "media",
 	theme: {
 		extend: {
 			backgroundImage: {
