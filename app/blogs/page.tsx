f287d992708bd9054d039276d184f0dfd1e60d78
Rahul <chattychidiya@gmail.com>
2024-10-06 19:43:10 +0530

    Add initial implementation of Blog and Projects pages

---
+++ app/blogs/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+function Blog({}: Props) {
+	return (
+		<main className="min-h-full w-full md:max-w-2xl mx-auto px-5 py-20 mb-20 flex flex-col gap-10 text-zinc-400 text-center">
+			Coming Soon....
+		</main>
+	);
+}
+
+export default Blog;
